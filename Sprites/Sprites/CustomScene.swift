6f12f1d110e3771d32dcc8170ef835350d6d19aa
jkaunert <jkaunert@clusterfly.net>
2018-11-05 16:17:53 -0600

    adds random color generation for each shape node

--- Sprites/Sprites/CustomScene.swift
+++ Sprites/Sprites/CustomScene.swift
@@ -7,8 +7,18 @@ class CustomScene: SKScene {
             else { return }
         
         let node = SKShapeNode(circleOfRadius: 8)
-        node.fillColor = .red
+
+        node.fillColor = randomColor()
         node.position = touch.location(in: self)
         addChild(node)
+    }
+    
+    // returns a random RGB color and alpha
+    func randomColor() -> UIColor {
+        let red = CGFloat.random(in: 0.0 ... 1.0)
+        let green = CGFloat.random(in: 0.0 ... 1.0)
+        let blue = CGFloat.random(in: 0.0 ... 1.0)
+        let alpha = CGFloat.random(in: 0.5 ... 1.0)
+        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
     }
 }
